ee4ddaff98560cc21fd3678e39debaf973130931
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 20:37:29 +0000

    7 que

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS cities
     id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
