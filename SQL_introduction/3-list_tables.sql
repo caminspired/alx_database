d30bd572e278a571b2685301ea43dc55e7fd96b3
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 17:13:07 +0000

    update

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
 -- this script lists all the tables of the MYSQL server --
 
 -- lists all the tables in a database passed as an argument --
-USE mysql;
+USE DATABSE IF DATABASE=mysql;
 SHOW tables; 
