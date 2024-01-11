89ab2d728775c38e723918cb630432b49e663044
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 17:56:20 +0000

    3 data

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,16 @@
 -- this script lists all the tables of the MYSQL server 
 
 -- define a variable for the passed database name 
-DEFINE @dbname NVARCHAR(255);
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
 
 -- set the database name to the desired name 
-SET @dbname='mysql';
+database_name="$1";
 
 -- use the database 
-USE @dbname;
+USE $database_name;
 
 -- show all tables from mysql 
 SHOW tables; 
