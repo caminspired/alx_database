0df3ed7f1fe027e811fcefb0a369e060125d12d1
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 19:15:54 +0000

    up 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- this script creates a table and adds multiple rows
 
-CREATE TABLE IF NOT EXISTS second_table(id int, name VARCHAR(256), score int); 
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT); 
 INSERT INTO second_table(id, name, score) VALUES
 (
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
-    (4, 'George', 8),
+    (4, 'George', 8)
 );
