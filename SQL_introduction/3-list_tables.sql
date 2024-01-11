51222b283fa9897c21f4778aa69b2c83f130ade8
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 18:09:40 +0000

    update

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,4 @@
 -- this script lists all the tables of the MYSQL server 
 
--- Check if the database name is provided as an argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
--- Assign the provided database name to a variable
-database_name="$1"
-
--- MySQL command to list all tables in the specified database
-mysql -hlocalhost -uroot -p -e "USE $database_name; SHOW TABLES;"
+-- show all tables from mysql 
+SHOW tables; 
