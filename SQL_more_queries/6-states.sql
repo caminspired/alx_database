f2af523c30e14395aedbeb994911b8d7390b920b
caminspired <charlotteaowusuansah@gmail.com>
2024-01-11 20:22:13 +0000

    6 que

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,5 +3,7 @@
 -- creates a new database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+
 -- creates a table
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
